24538620d41685deeebee30f9a56fa64891a0235
victory <vickywane@gmail.com>
2018-12-18 18:58:45 -0500

    Add files via upload

---
+++ Contacts.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { View, Text, ActivityIndicator } from 'react-native';
+
+import { graphql, Query } from 'react-apollo'
+import gql from 'graphql-tag'
+
+class Contacts extends Component {
+  render() {
+    return (
+      <Query query={getContacts} >
+
+        {({ loading, error, data: { contacts } }
+        ) => {
+          if (loading) {
+            return (
+              <ActivityIndicator />
+            );
+          }
+          
+          if (error) {
+            return "An error has ocurred";
+          }
+
+          return contacts.map(contact => (
+            <View>
+              <Text> {contact.contacts.firstname} </Text>
+            </View>
+          ))
+        }
+        });
+      </Query>
+    );
+  }
+}
+
+const getContacts = gql`
+ query {
+   contacts {
+    id
+    firstname
+    lastname
+     }
+  }
+ `
+
+const ContactsWrapper = graphql(getContacts)(Contacts)
+export default ContactsWrapper
